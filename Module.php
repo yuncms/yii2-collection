dc2785038458a626752c48e2f43fab0ecf631095
Chao Xu <xutongle@gmail.com>
2017-08-27 12:12:45 +0800

    init

---
+++ Module.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace yuncms\collection;
+
+
+use Yii;
+
+/**
+ * Class Module
+ * @package yuncms\credit
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * 初始化
+     */
+    public function init()
+    {
+        parent::init();
+        $this->registerTranslations();
+    }
+
+    /**
+     * 注册语言包
+     * @return void
+     */
+    public function registerTranslations()
+    {
+        if (!isset(Yii::$app->i18n->translations['collection*'])) {
+            Yii::$app->i18n->translations['collection*'] = [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'sourceLanguage' => 'en-US',
+                'basePath' => __DIR__ . '/messages',
+            ];
+        }
+    }
+}
